052de0dbced129ff340f4053b4305e4543441e9e
jayeshdhage07 <jshdhage@gmail.com>
2024-09-15 12:42:04 +0530

    Add project files.

---
+++ BankingSystem.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp
+{
+    internal class BankingSystem
+    {
+        public string? bankAccName;
+        public int? accNumber;
+        public int? balance;
+
+        public void BankAccount()
+        {
+            balance = 250000;
+        }
+
+        public void createAccount()
+        {
+            Console.WriteLine("Enter bank Bank name");
+            string? userInput1 = Console.ReadLine();
+            bankAccName = userInput1;
+
+            Console.WriteLine("Enter bank account number");
+            string? userInput2 = Console.ReadLine();
+            accNumber = Convert.ToInt32(userInput2);
+            Console.WriteLine($"Enter account has been created details are \n {bankAccName}, \n {accNumber} ");
+            Console.WriteLine("--------------------------------------------");
+        }
+
+        public void checkAccount()
+        {
+            if(accNumber != null && bankAccName != null)
+            {
+                Console.WriteLine($"Your bank account name is: {bankAccName} and bank account no is {accNumber}");
+            }
+        }
+
+        public void deposit()
+        {
+            Console.WriteLine("Enter Enter account details where you want to deposit");
+            checkAccount();
+            Console.WriteLine("Enter amount you want to deposit");
+            string? userInput3 = Console.ReadLine();
+            balance = Convert.ToInt32(userInput3);
+        }
+
+        public void withdraw()
+        {
+            Console.WriteLine("Enter Enter account details where you want to withdraw");
+            checkAccount();
+            Console.WriteLine("Enter amount you want to withdraw");
+            string? userInput4 = Console.ReadLine();
+
+            if(Convert.ToInt32(userInput4) < 0 && Convert.ToInt32(userInput4) <= balance)
+            {
+                Console.WriteLine("you don't have sufficient balance");
+            }
+            else
+            {
+                balance = balance - Convert.ToInt32(userInput4);
+                Console.WriteLine($"You withdraw {Convert.ToInt32(userInput4)}, remaining balance is {balance}");
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ConsoleApp 
+{ 
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Your Bank System");
+
+            BankingSystem myBank = new BankingSystem();
+            myBank.createAccount();
+            myBank.deposit();
+            myBank.withdraw();
+        }
+    }
+}
